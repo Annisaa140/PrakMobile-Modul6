794a423daac8bf3279e192a5c4a881aa151d2b54
Annisaa140 <annisaan7@gmail.com>
2020-05-14 11:28:06 +0700

    mhsCRUD

---
+++ mhsCRUD/app/src/main/java/com/example/mhscrud/konfigurasi.java
@@ -0,0 +1,22 @@
+package com.example.mhscrud;
+
+public class konfigurasi {
+    public static final String URL_ADD="http://192.168.43.167/Android/Mahasiswa/insert.php";
+    public static final String URL_GET_ALL="http://192.168.43.167/Android/Mahasiswa/read.php";
+    public static final String URL_GET_MHS="http://192.168.43.167/Android/Mahasiswa/select.php?id=";
+    public static final String URL_UPDATE_MHS="http://192.168.43.167/Android/Mahasiswa/update.php";
+    public static final String URL_DELETE_MHS="http://192.168.43.167/Android/Mahasiswa/delete.php?id=";
+
+    public static final String KEY_MHS_ID = "id";
+    public static final String KEY_MHS_NAMA = "nama";
+    public static final String KEY_MHS_JURUSAN = "jurusan";
+    public static final String KEY_MHS_EMAIL = "email";
+
+    public static final String TAG_JSON_ARRAY = "result";
+    public static final String TAG_ID = "id";
+    public static final String TAG_NAMA = "nama";
+    public static final String TAG_JURUSAN = "jurusan";
+    public static final String TAG_EMAIL = "email";
+
+    public static final String MHS_ID = "mhs_id";
+}
